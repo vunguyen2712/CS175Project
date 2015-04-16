0e91931cce07f1ce4e80c65b5e8405940e739421
brianpaff1992 <brianpaff1992@gmail.com>
2015-04-15 21:45:50 -0700

    Re

--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -4,7 +4,6 @@ public class MazeSolver {
 	public static void main(String[] args)
 	{
 		Maze maze = new Maze(2,2);
-		Maze maze = new Maze(4,4);
 		Window window = new Window();
 		window.render(maze);
 		//while(!done)
